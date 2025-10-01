6dc7efcb1fdc96ebce490269ea7fc39ad99b1340
florian-lup <contact@florianup.com>
2025-10-01 10:54:26 +0100

    feat: Change theme configuration to enforce dark mode in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          forcedTheme="dark"
           disableTransitionOnChange
         >
           {children}
